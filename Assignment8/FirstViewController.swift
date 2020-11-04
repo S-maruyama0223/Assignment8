e94361a2f7be9d316c85db9ddd05e2e0760cbe10
S-maruyama0223 <wanshanxiangtailang0223@gmail.com>
2020-11-04 21:20:22 +0900

    Appdelegate使用で実装

--- Assignment8/FirstViewController.swift
+++ Assignment8/FirstViewController.swift
@@ -11,25 +11,16 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var valueLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        valueLabel.text = String(slider.value)
-    }
-    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
     @IBAction private func valueChanged(_ slider: UISlider) {
         valueLabel.text = String(slider.value)
+        appDelegate.sliderValue = slider.value
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        guard let destinationIndex = tabBarController?.viewControllers?.firstIndex(where: { $0 is SecondViewController })
-        else{
-            return
-        }
-        let secondViewController = tabBarController!.viewControllers![destinationIndex] as! SecondViewController
-        secondViewController.loadViewIfNeeded()
-        self.slider.value = secondViewController.slider.value
-        self.valueLabel.text = secondViewController.valueLabel.text
+        self.slider.value = appDelegate.sliderValue
+        self.valueLabel.text = String(appDelegate.sliderValue)
     }
 }
--- Assignment8/SecondViewController.swift
+++ Assignment8/SecondViewController.swift
@@ -11,26 +11,17 @@ class SecondViewController: UIViewController {
     
     @IBOutlet weak var valueLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        valueLabel.text = String(slider.value)
-    }
-    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
     @IBAction private func valueChanged(_ slider: UISlider) {
         valueLabel.text = String(slider.value)
+        appDelegate.sliderValue = slider.value
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        guard let destinationIndex = tabBarController?.viewControllers?.firstIndex(where: { $0 is FirstViewController })
-        else{
-            return
-        }
-        let firstViewController = tabBarController!.viewControllers![destinationIndex] as! FirstViewController
-        firstViewController.loadViewIfNeeded()
-        self.valueLabel.text = firstViewController.valueLabel.text
-        self.slider.value = firstViewController.slider.value
+        self.slider.value = appDelegate.sliderValue
+        self.valueLabel.text = String(appDelegate.sliderValue)
     }
 
 }
